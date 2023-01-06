801bc165c030e06cbb9e9fed2218d6bee7815028
gigip17 <pierre@MacBook-Air-de-Pierre.local>
2023-01-06 16:07:36 +0100

    oui

--- 02_tagSelector.js
+++ 02_tagSelector.js
@@ -1,9 +1,5 @@
 function tagSelector() {
-    const button = document.querySelector('button');
-    if (button) {
-      return button.outerHTML;
-    }
-    return null;
+    return document.querySelector('button');
   }
 console.log(tagSelector()); // element>button 
 console.log(tagSelector().innerHTML); //"hello"
