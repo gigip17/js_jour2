d2f4bd5d88ec60df263dc0bc08ecc6b892075cd9
gigip17 <pierre@MacBook-Air-de-Pierre.local>
2023-01-09 15:20:58 +0100

    exo11

--- 11_domInjection.js
+++ 11_domInjection.js
@@ -1,8 +1,7 @@
-function insertHelloWorld(parent) {
+function insertHelloWorld() {
+    var content = document.getElementById("content");
     var p = document.createElement("p");
     p.innerText = "Hello World";
-    parent.insertBefore(p, target.firstChild);
+    content.insertBefore(p, content.firstChild);
   }
-  var content = document.getElementById("content");
-  insertHelloWorld(content);
-  
+  insertHelloWorld();
