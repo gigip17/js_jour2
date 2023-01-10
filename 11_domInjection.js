4077b59017514e059754ae363b1f9bfbc76ba501
gigip17 <pierre@MacBook-Air-de-Pierre.local>
2023-01-10 15:50:53 +0100

    exo14

--- 11_domInjection.js
+++ 11_domInjection.js
@@ -1,7 +1,7 @@
 function insertHelloWorld() {
     var content = document.getElementById("content");
     var p = document.createElement("p");
-    p.innerText = "hello World";
+    p.innerText = "hello world";
     content.insertBefore(p, content.firstChild);
   }
   insertHelloWorld();
