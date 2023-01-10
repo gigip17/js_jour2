582fd7b2aa3c86e2da63ec0f9e2defd2b93bc166
gigip17 <pierre@MacBook-Air-de-Pierre.local>
2023-01-10 10:51:10 +0100

    exo14

---
+++ 16_contactForm.js
@@ -0,0 +1,13 @@
+const submitButton = document.querySelector("button#submit");
+const formElements = document.querySelectorAll("form input");
+const result = document.querySelector("p#result");
+
+submitButton.addEventListener("click", function() {
+  let values = [];
+  for (let i = 0; i < formElements.length; i++) {
+    let name = formElements[i].name;
+    let value = formElements[i].value;
+    values.push(`${name}: ${value}`);
+  }
+  result.textContent = values.join(", ");
+});
